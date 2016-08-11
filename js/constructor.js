250d04b19ee0dcb6e139ba33a6366b0afb5fd95b
Daniel Dominguez <dannickfox@gmail.com>
2016-08-11 12:43:10 -0500

    constructor file and function

---
+++ js/constructor.js
@@ -0,0 +1,14 @@
+// constructor.js
+// creates objects for phyzzy mesh
+'use strict'
+
+const Mass = require('./phyzzy/components/mass.js')
+const Spring = require('./phyzzy/components/spring.js')
+
+const Construct = (mouse, phyzzy) => {
+    const mCount, sCount
+
+    return mCount
+}
+
+module.exports = Construct
