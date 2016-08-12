a88dc490e3902df7781aaf8137522461a6b5c05f
Daniel Dominguez <dannickfox@gmail.com>
2016-08-12 14:22:23 -0500

    constructor changes

--- phMain.js
+++ phMain.js
@@ -1,11 +1,9 @@
 // phyzzy main webapp
 'use strict'
 const Phyzzy = require('./js/phyzzy/engine.js')
-const Mass = require('./js/phyzzy/components/mass.js')
-const Spring = require('./js/phyzzy/components/spring.js')
 const Environment = require('./js/phyzzy/components/environment.js')
 const User = require('./js/user.js')
-const Construct = require('./js/constructor.js')
+const MeshBuilder = require('./js/constructor.js')
 
 const viewport = document.getElementById('viewport')
 const ctx = viewport.getContext('2d')
@@ -18,45 +16,22 @@ const env = Environment(
     {x: 0, y: 0, w: viewport.width / ph.scale, h: viewport.height / ph.scale}
 )
 
-/*
-const m1 = Mass(
-        {mass: 0.1, rad: 0.05, refl: 0.75, mu_s: 0.0, mu_k: 0.2},
-        {x: viewport.width / 2 / ph.scale, y: 0.05},
-        {x: 2.4, y: 0.05}
-    )
-const m2 = Mass(
-        {mass: 0.1, rad: 0.05, refl: 0.75, mu_s: 0.0, mu_k: 0.2},
-        {x: viewport.width / 2 / ph.scale, y: 0.05},
-        {x: 2.6, y: 0.05}
-    )
-const m3 = Mass(
-        {mass: 0.1, rad: 0.05, refl: 0.75, mu_s: 0.0, mu_k: 0.2},
-        {x: viewport.width / 2 / ph.scale, y: 0.05},
-        {x: 2.6, y: 0.1}
-    )
-const s1 = Spring(1, 100, 50)
-const s2 = Spring(1, 100, 50)
-const s3 = Spring(1, 100, 50)
-
-ph.addM(m1)
-ph.addM(m2)
-ph.addM(m3)
-ph.addS(m1, m2, s1)
-ph.addS(m2, m3, s2)
-ph.addS(m3, m1, s3)
-*/
-
 const mouse = User.Mouser(ph.scale)
+const builder = MeshBuilder()
 
 mouse.init(viewport, ph)
+builder.enableSet(true)
+builder.massPropSet(0.1, 0.05, 0.8, 0.0, 0.4)
 
-const frame = (frameTime) => {
+const frame = () => {
     ctx.clearRect(0, 0, viewport.width, viewport.height)
 
     ph.drawSpring(ctx, '#000000')
     ph.drawMass(ctx, '#1DB322')
     mouse.hover(ph, ctx, '#1DB322')
     mouse.select(ctx)
+    builder.buildMass(ph, mouse)
+
     ph.verlet(
         ph.m.map(mass => {
             let f = env.weight(mass)
